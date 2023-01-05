038a52e8a049a4b9f26c259a810d864896c2de09
Daim <muslim_abdulov@mail.ru>
2023-01-05 14:47:17 -0800

    Исправлен метод getStatus

--- tamogotchi.js
+++ tamogotchi.js
@@ -4,46 +4,50 @@ const tamogotchi = {
     energy: 8,
     mood: 8,
 
-   getStatus() {
-       if (this.meal >= 5 && this.energy >= 5 && this.mood >= 5) {
-           console.log(`${this.name} счастлив.`);
-           return;
-       }
-       
-       if (this.meal <= 0 || this.energy <= 0 || this.mood <= 0) {
-           console.log(`${this.name} умер.`);
-           return;
-       }
-      
-       
-    if (this.meal < 3) {
-      this.meal = `еда: Я голоден(${this.meal})`;
-    } else {
-        this.meal = `еда: Я не голоден(${this.meal}`;
-    }
-    
-    if (this.energy < 3) {
-        this.energy = `энергия: Я устал(${this.energy})`}
-     else {
-        this.energy = `энергия  Я не устал(${this.energy})`};
-        
-    if (this.mood < 3) {
-        this.mood = `настроение: Мне грустно(${this.mood})`;
-        
-    } else 
-        {this.mood = `настроение: Мне весело(${this.mood})`};
-        
-        
-        
-        console.log(`имя: ${this.name}, ${this.meal}, ${this.energy}, ${this.mood}`);
+    getStatus() {
+        let meal = '';
+        let energy = '';
+        let mood = '';
+
+        if (this.meal >= 5 && this.energy >= 5 && this.mood >= 5) {
+            console.log(`${this.name} счастлив.`);
+        }
+
+        if (this.meal <= 0 || this.energy <= 0 || this.mood <= 0) {
+            console.log(`${this.name} умер.`);
+            return;
+        }
+
+
+        if (this.meal < 3) {
+            meal = `еда: Я голоден(${this.meal})`;
+        } else {
+            meal = `еда: Я не голоден(${this.meal}`;
+        }
+
+        if (this.energy < 3) {
+            energy = `энергия: Я устал(${this.energy})`
+        }
+        else {
+            energy = `энергия  Я не устал(${this.energy})`
+        };
+
+        if (this.mood < 3) {
+            mood = `настроение: Мне грустно(${this.mood})`;
+
+        } else { mood = `настроение: Мне весело(${this.mood})` };
+
+
+
+        console.log(`имя: ${this.name}, ${meal}, ${energy}, ${mood}`);
     },
 
     setName(n) {
-        this.name = n; 
+        this.name = n;
     },
 
     eat() {
-        if(this.meal < 5) {
+        if (this.meal < 5) {
             this.meal++;
             this.mood--;
         }
